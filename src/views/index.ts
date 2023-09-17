f906b66b96fc477b3fda5f05a95e0ef3bf02f950
Anton Cristo <antoncristo92@gmail.com>
2023-09-17 10:15:53 +0300

    ReadRecipe component added to routes

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,3 +1,4 @@
 export { Login } from './login';
 export { Dashboard } from './dashboard';
 export { Wizard } from './wizard';
+export { ReadRecipe } from './read-recipe';
---
+++ src/views/read-recipe/read-recipe.tsx
@@ -0,0 +1,3 @@
+import * as Styled from './read-recipe.styled';
+
+export const ReadRecipe = () => <div>ReadRecipe works!</div>;
