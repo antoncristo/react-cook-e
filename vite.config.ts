1ebeb208360f27ac32dae942fc15b79793bf3079
Anton Cristo <antoncristo92@gmail.com>
2023-09-17 17:31:15 +0300

    global enhancements

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
+	resolve: {
+		alias: {
+			'@cooke': resolve(__dirname, 'src')
+		}
+	},
 	test: {
 		name: 'ui-unit-tests',
 		globals: true,
