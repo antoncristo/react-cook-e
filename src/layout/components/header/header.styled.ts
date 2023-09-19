d926635087ae7eec0bc12726fe500a67863003b3
Anton Cristo <antoncristo92@gmail.com>
2023-09-19 14:57:25 +0300

    layout header component

---
+++ src/layout/components/header/header.styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+	box-sizing: border-box;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
---
+++ src/layout/components/header/header.tsx
@@ -0,0 +1,9 @@
+import { CookeLogo } from '@cooke/shared';
+
+import * as Styled from './header.styled';
+
+export const Header = () => (
+	<Styled.Header>
+		<CookeLogo fontSize='4rem' />
+	</Styled.Header>
+);
