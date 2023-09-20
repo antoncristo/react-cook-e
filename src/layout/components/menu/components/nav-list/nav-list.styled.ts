be3a54f38a0e4ff8f0e01276b86c8232330fa8f5
Anton Cristo <antoncristo92@gmail.com>
2023-09-20 13:03:44 +0300

    menu nav bar with links

---
+++ src/layout/components/menu/components/nav-list/nav-list.styled.ts
@@ -0,0 +1,34 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const NavList = styled.nav`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	gap: 1.6rem;
+`;
+
+export const MenuLink = styled(NavLink)`
+	box-sizing: border-box;
+	font-size: 1.4rem;
+	text-decoration: none;
+	color: rgba(${({ theme }) => theme.colors.ternary}, 1);
+	height: 3rem;
+	display: flex;
+	align-items: center;
+	justify-content: flex-start;
+	padding: 0 ${({ theme }) => theme.decorators.padding.default};
+	width: 100%;
+	border-bottom: 0.1rem dashed transparent;
+	transition: all 0.26s ease-in-out;
+
+	&.active,
+	&:hover {
+		font-family: 'cooke-600';
+		border-bottom: 0.1rem dashed rgba(${({ theme }) => theme.colors.ternary}, 1);
+	}
+
+	&:focus {
+		outline: none;
+	}
+`;
--- src/layout/components/menu/menu.tsx
+++ src/layout/components/menu/menu.tsx
@@ -1,3 +1,8 @@
+import { NavList } from './components';
 import * as Styled from './menu.styled';
 
-export const Menu = () => <Styled.Menu>Menu works!</Styled.Menu>;
+export const Menu = () => (
+	<Styled.Menu>
+		<NavList />
+	</Styled.Menu>
+);
