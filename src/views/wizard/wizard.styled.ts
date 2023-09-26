5f44bc466003d8db04f757fbc924a0eafca20116
Anton Cristo <antoncristo92@gmail.com>
2023-09-26 09:41:25 +0300

    dish name input

--- src/views/wizard/wizard.styled.ts
+++ src/views/wizard/wizard.styled.ts
@@ -1,3 +1,49 @@
+import { Input } from '@cooke/shared';
 import styled from 'styled-components';
 
-export const Wizard = styled.div``;
+export const Wizard = styled.div`
+	width: 100%;
+	height: 100%;
+
+	display: flex;
+	align-items: flex-start;
+	justify-content: center;
+`;
+
+export const WizardFlex = styled.div`
+	box-sizing: border-box;
+	width: 100%;
+	height: 100%;
+	max-width: 99rem;
+	padding-inline-end: 6rem;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	justify-content: flex-start;
+	gap: 1.6rem;
+	overflow-y: auto;
+
+	scrollbar-width: none;
+	&::-webkit-scrollbar {
+		display: none;
+	}
+`;
+
+export const WizardTitle = styled.h1`
+	font-family: 'cooke-600';
+	font-size: 3rem;
+	color: rgba(${({ theme }) => theme.colors.primary}, 1);
+	text-shadow: ${({ theme }) => theme.decorators.textShadow.primary};
+	border-bottom: 0.1rem dashed rgba(${({ theme }) => theme.colors.ternary}, 1);
+	margin: 0;
+`;
+
+export const WizardTitleInput = styled(Input)`
+	background-color: rgba(${({ theme }) => theme.colors.secondary}, 0.6);
+	color: rgba(${({ theme }) => theme.colors.white}, 1);
+	width: 100%;
+
+	&::placeholder {
+		color: rgba(${({ theme }) => theme.colors.ternary}, 0.9);
+	}
+`;
--- src/views/wizard/wizard.tsx
+++ src/views/wizard/wizard.tsx
@@ -1,3 +1,10 @@
 import * as Styled from './wizard.styled';
 
-export const Wizard = () => <div>Wizard works!</div>;
+export const Wizard = () => (
+	<Styled.Wizard>
+		<Styled.WizardFlex>
+			<Styled.WizardTitle>What`s Cook-E ?</Styled.WizardTitle>
+			<Styled.WizardTitleInput placeholder='Dish name...' />
+		</Styled.WizardFlex>
+	</Styled.Wizard>
+);
