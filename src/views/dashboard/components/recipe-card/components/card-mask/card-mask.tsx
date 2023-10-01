18830bd3666e59493e9a1a6490c342529184e41d
Anton Cristo <antoncristo92@gmail.com>
2023-10-01 10:49:17 +0300

    dashboard recipe card mask

---
+++ src/views/dashboard/components/recipe-card/components/card-mask/card-mask.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import { Button } from '@cooke/shared';
+import { type Recipe } from '@cooke/types';
+import { cookePathnames, injectRecipeUUID } from '@cooke/router';
+
+import * as Styled from './card-mask.styled';
+
+export const CardMask = ({ recipe }: { recipe: Recipe }) => (
+	<Styled.CardMask className='mask'>
+		<Styled.LinkButton
+			to={injectRecipeUUID(cookePathnames.authenticatedPathNames.READ_RECIPE, recipe.id)}
+		>
+			Go To Recipe
+		</Styled.LinkButton>
+		<Styled.LinkButton style={{ pointerEvents: 'none' }} to=''>
+			Edit
+		</Styled.LinkButton>
+	</Styled.CardMask>
+);
