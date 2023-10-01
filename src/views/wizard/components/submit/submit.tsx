1f17b55ef44b4bc0b66f2afc07704af235688ea6
Anton Cristo <antoncristo92@gmail.com>
2023-10-01 13:17:22 +0300

    wizard submit splitt into new or udpated recipe

--- src/views/wizard/components/submit/submit.tsx
+++ src/views/wizard/components/submit/submit.tsx
@@ -3,23 +3,28 @@ import { useNavigate } from 'react-router-dom';
 import { Button } from '@cooke/shared';
 import { wizardStore } from '@cooke/stores/wizard-store';
 import { wizardValidator } from '@cooke/stores/wizard-validator';
-import { usePostRecipe } from '@cooke/api/recipe';
+import { usePostRecipe, usePutRecipe } from '@cooke/api/recipe';
 import { cookePathnames } from '@cooke/router';
 
 export const Submit = observer(() => {
-	const { recipe } = wizardStore;
-	const { postRecipe, isError } = usePostRecipe();
+	const { recipe, isEditMode } = wizardStore;
+	const { postRecipe } = usePostRecipe();
+	const { putRecipe } = usePutRecipe();
 	const navigate = useNavigate();
 
 	const onSubmit = () => {
-		// eslint-disable-next-line
-		if (!wizardValidator.isRecipeValid()) {
+		if (wizardValidator.isRecipeValid()) {
+			if (isEditMode) {
+				putRecipe(recipe);
+			} else {
+				postRecipe(recipe);
+			}
+
+			navigate(cookePathnames.authenticatedPathNames.DASHBOARD);
+		} else {
 			wizardValidator.touchAll();
 			// eslint-disable-next-line
 			alert('Please fill correctly all the highlighted/missing fields');
-		} else {
-			postRecipe(recipe);
-			navigate(cookePathnames.authenticatedPathNames.DASHBOARD);
 		}
 	};
 
