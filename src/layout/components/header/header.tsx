b601d50cc2f8a7e2dbfea1c3f73f08cb5d8c2156
Anton Cristo <57062626+antoncristo@users.noreply.github.com>
2023-10-03 15:01:20 +0300

    icons (#33)
    icon assets applied

--- src/layout/components/header/header.tsx
+++ src/layout/components/header/header.tsx
@@ -4,6 +4,6 @@ import * as Styled from './header.styled';
 
 export const Header = () => (
 	<Styled.Header>
-		<CookeLogo fontSize='4rem' />
+		<CookeLogo fontSize='4rem' iconDiameter={40} />
 	</Styled.Header>
 );
--- src/shared/cooke-logo/cooke-logo.styled.ts
+++ src/shared/cooke-logo/cooke-logo.styled.ts
@@ -2,10 +2,13 @@ import styled from 'styled-components';
 
 export const CookeLogo = styled.div<{ $fontSize: string }>`
 	font-size: ${({ $fontSize }) => $fontSize};
+	line-height: 1;
 	font-family: 'cooke-600';
 	color: rgba(${({ theme }) => theme.colors.ternary}, 1);
 	text-shadow: 0.3rem 0.3rem 0.6rem rgba(${({ theme }) => theme.colors.black}, 1);
 	transition: all 0.2s ease-in-out;
+	display: flex;
+	align-items: center;
 
 	cursor: default;
 
