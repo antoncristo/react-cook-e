0ef3ecbd8a7e456004b062b666b5ba4cdbba2030
Anton Cristo <antoncristo92@gmail.com>
2023-10-04 08:48:26 +0300

    navigate to dashboard on logo click

--- src/shared/cooke-logo/cooke-logo.tsx
+++ src/shared/cooke-logo/cooke-logo.tsx
@@ -1,4 +1,7 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import { cookePathnames } from '@cooke/router';
 import { DoughnutIcon } from '@cooke/assets';
+
 import * as Styled from './cooke-logo.styled';
 
 interface CookeLogoProps {
@@ -8,9 +11,17 @@ interface CookeLogoProps {
 
 export const CookeLogo = (props: CookeLogoProps) => {
 	const { fontSize, iconDiameter } = props;
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	const onLogoClick = () => {
+		if (!location.pathname.includes('dashboard')) {
+			navigate(cookePathnames.authenticatedPathNames.DASHBOARD);
+		}
+	};
 
 	return (
-		<Styled.CookeLogo $fontSize={fontSize}>
+		<Styled.CookeLogo onClick={onLogoClick} $fontSize={fontSize}>
 			<DoughnutIcon width={iconDiameter} height={iconDiameter} right='1rem' />
 			CooK-E
 		</Styled.CookeLogo>
