d8583d273a1a982774f8c8bcc3d9712a034a4a10
Anton Cristo <antoncristo92@gmail.com>
2023-10-06 09:17:44 +0300

    set api base url per environment

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -15,6 +15,7 @@ interface LayoutProps {
 export const Layout = observer(({ children }: LayoutProps) => {
 	const { user } = userStore;
 
+	// Fix: clear logs after data is verified
 	// eslint-disable-next-line no-console
 	console.log(import.meta.env);
 	// eslint-disable-next-line no-console
