16e279a7f3ad977708ab4e2ee89c20507d6fd95a
Anton Cristo <antoncristo92@gmail.com>
2023-10-06 17:16:20 +0300

    put recipe service

--- src/api/recipe/recipe.interface.ts
+++ src/api/recipe/recipe.interface.ts
@@ -2,8 +2,9 @@ import { type QueryBasicParams, type Recipe } from '@cooke/types';
 import { type AxiosResponse } from 'axios';
 
 export interface RecipeServiceApi {
+	routePath: string;
 	getRecipes: (queryParams: QueryBasicParams) => Promise<AxiosResponse<Recipe[]>>;
 	postRecipe: (recipe: Recipe) => Promise<AxiosResponse<Recipe>>;
-	putRecipe: (recipe: Recipe) => Promise<Recipe>;
+	putRecipe: (updatedRecipe: Recipe) => Promise<AxiosResponse<Recipe>>;
 	deleteRecipe: (recipeToDelete: Recipe) => Promise<AxiosResponse<UUID>>;
 }
--- src/api/recipe/recipe.service.ts
+++ src/api/recipe/recipe.service.ts
@@ -1,101 +1,14 @@
-import { type Recipe } from '@cooke/types';
-import { type RecipeServiceApi } from './recipe.interface';
-import { type QueryBasicParams } from '@cooke/types';
 import { type AxiosResponse } from 'axios';
+import { type Recipe } from '@cooke/types';
+import { type QueryBasicParams } from '@cooke/types';
+
+import { type RecipeServiceApi } from './recipe.interface';
 
 import { axiosClient } from '../axios';
 
-const recipes: Recipe[] = [
-	{
-		id: crypto.randomUUID(),
-		title: 'Shnitzel overflowing text test overflowing text test overflowing text test',
-		description:
-			'Deep fried chicken breasts overflowing\n text test overflowing text test overflowing text test overflowing text testoverflowing text test overflowing text test overflowing text testoverflowing text test overflowing text test overflowing text testoverflowing text test overflowing text test overflowing text testoverflowing text test overflowing text test overflowing text testoverflowing text test overflowing text test overflowing text testoverflowing text test overflowing text test overflowing text testoverflowing text test overflowing text test overflowing text testoverflowing text test overflowing text test overflowing text testoverflowing text test overflowing text test overflowing text test',
-		ingredients: [
-			{
-				id: '1-1-1-1',
-				name: 'Oil',
-				amount: 1,
-				unit: 'l'
-			},
-			{
-				id: '1-1-1-2',
-				name: 'Chicken breast',
-				amount: 1,
-				unit: 'kg'
-			},
-			{
-				id: '1-1-1-3',
-				name: 'Egg',
-				amount: 1,
-				unit: 'unit'
-			},
-			{
-				id: '1-1-1-4',
-				name: 'Breadcrumbs',
-				amount: 1,
-				unit: 'unit'
-			},
-			{
-				id: '1-1-1-5',
-				name: 'Mustard',
-				amount: 60,
-				unit: 'g'
-			}
-		],
-		steps: [
-			{
-				id: '1-1-1-1',
-				stepCount: 1,
-				description: 'Cut the chicken breast into small semi flat square pieces'
-			},
-			{
-				id: '1-1-1-2',
-				stepCount: 2,
-				description:
-					'Soak the cubes in a mix of mustard,mayo,salt,pepper and eggs for ~1h'
-			},
-			{
-				id: '1-1-1-3',
-				stepCount: 3,
-				description:
-					'Prepare a breadcrumbs mix: breadcrumbs, chicken soup powder, smoked paprika, garlic powder'
-			},
-			{
-				id: '1-1-1-4',
-				stepCount: 4,
-				description: 'Coat the soaked cubes with the breadcrumbs mix and cool for ~30mins'
-			},
-			{
-				id: '1-1-1-5',
-				stepCount: 5,
-				description: 'Deep fry until golden'
-			}
-		]
-	},
-	{
-		id: crypto.randomUUID(),
-		title: 'Bologniez',
-		description: 'Spaghetti with beef and tomato sauce overflowing text test',
-		ingredients: [
-			{
-				id: '1-2-1-1',
-				amount: 1,
-				name: 'Beaf',
-				unit: 'kg'
-			}
-		],
-		steps: [
-			{
-				id: '1-1-1-1',
-				description: 'Make it',
-				stepCount: 1
-			}
-		]
-	}
-];
-
 class RecipeService implements RecipeServiceApi {
+	routePath: string = '/recipes';
+
 	getRecipes = async (
 		queryParams?: QueryBasicParams
 	): Promise<AxiosResponse<Recipe[]>> => {
@@ -103,29 +16,17 @@ class RecipeService implements RecipeServiceApi {
 			search: queryParams?.search ? queryParams?.search : undefined
 		};
 
-		return axiosClient.get('/recipes', { params });
+		return axiosClient.get(this.routePath, { params });
 	};
 
 	postRecipe = async (recipe: Recipe): Promise<AxiosResponse<Recipe>> =>
-		axiosClient.post('/recipes', { recipe });
+		axiosClient.post(this.routePath, { recipe });
 
-	putRecipe = async (recipe: Recipe): Promise<Recipe> => {
-		let needsUpdate = recipes.find(r => r.id === recipe.id);
-		if (needsUpdate) {
-			needsUpdate = JSON.parse(JSON.stringify({ ...needsUpdate, ...recipe }));
-
-			recipes.forEach((r, index) => {
-				if (r.id === needsUpdate?.id) {
-					recipes[index] = needsUpdate;
-				}
-			});
-		}
-
-		return Promise.resolve(needsUpdate!);
-	};
+	putRecipe = async (updatedRecipe: Recipe): Promise<AxiosResponse<Recipe>> =>
+		axiosClient.put(this.routePath, { updatedRecipe });
 
 	deleteRecipe = async (recipeToDelete: Recipe): Promise<AxiosResponse<UUID>> =>
-		axiosClient.delete(`/recipes/${recipeToDelete.id}`);
+		axiosClient.delete(`${this.routePath}/${recipeToDelete.id}`);
 }
 
 export const recipeService = new RecipeService();
