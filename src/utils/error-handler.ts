8b237706cb9e77a46b517bdca7dc43f4dc47d68e
Anton Cristo <antoncristo92@gmail.com>
2023-10-11 10:34:48 +0300

    error handler setup and usage - initial

---
+++ src/utils/error-handler.ts
@@ -0,0 +1,8 @@
+import { alertStore } from '@cooke/stores/alert-store';
+import { AxiosError } from 'axios';
+
+export const errorHandler = (err: unknown) => {
+	if (err instanceof AxiosError) {
+		alertStore.setAlert({ msg: err.response?.data });
+	}
+};
--- src/views/wizard/components/submit/submit.tsx
+++ src/views/wizard/components/submit/submit.tsx
@@ -1,15 +1,16 @@
 import { observer } from 'mobx-react-lite';
 import { useNavigate } from 'react-router-dom';
-import { Button } from '@cooke/shared';
+import { Button, Loader } from '@cooke/shared';
 import { wizardStore, wizardValidator } from '@cooke/stores/wizard-store';
 import { usePostRecipe, usePutRecipe } from '@cooke/api/recipe';
 import { cookePathnames } from '@cooke/router';
 import { alertStore } from '@cooke/stores/alert-store';
+import { useEffect } from 'react';
 
 export const Submit = observer(() => {
 	const { recipe, isEditMode } = wizardStore;
-	const { postRecipe } = usePostRecipe();
-	const { putRecipe } = usePutRecipe();
+	const { postRecipe, isPostLoading, isPostSuccess } = usePostRecipe();
+	const { putRecipe, isPutLoading, isPutSuccess } = usePutRecipe();
 	const navigate = useNavigate();
 
 	const onSubmit = () => {
@@ -19,8 +20,6 @@ export const Submit = observer(() => {
 			} else {
 				postRecipe(recipe);
 			}
-
-			navigate(cookePathnames.authenticatedPathNames.DASHBOARD);
 		} else {
 			wizardValidator.touchAll();
 			alertStore.setAlert({
@@ -29,9 +28,19 @@ export const Submit = observer(() => {
 		}
 	};
 
+	useEffect(() => {
+		if (isPostSuccess || isPutSuccess) {
+			navigate(cookePathnames.authenticatedPathNames.DASHBOARD);
+		}
+	}, [isPostSuccess, isPutSuccess]);
+
 	return (
 		<Button width='100%' onClick={onSubmit}>
-			<b>{isEditMode ? 'UPDATE' : 'SUBMIT'}</b>
+			{isPostLoading || isPutLoading ? (
+				<Loader size='S' />
+			) : (
+				<b>{isEditMode ? 'UPDATE' : 'SUBMIT'}</b>
+			)}
 		</Button>
 	);
 });
