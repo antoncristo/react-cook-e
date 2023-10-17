14d2c9d4528eb8e8dd1f825dc6a39df252bfe0ff
Anton Cristo <antoncristo92@gmail.com>
2023-10-17 12:22:23 +0300

    test interceptor

--- src/api/axios/utils.ts
+++ src/api/axios/utils.ts
@@ -4,10 +4,12 @@ import { type InternalAxiosRequestConfig } from 'axios';
 export const setAuthBearer = (request: InternalAxiosRequestConfig<unknown>) => {
 	const accessToken = getSessionStorage('token');
 	// Fix: test
+	// eslint-disable-next-line
 	console.log('extracted token', accessToken);
 	if (accessToken) {
 		// Fix: test
 		request.headers.Authorization = `Bearer ${accessToken}`;
+		// eslint-disable-next-line
 		console.log('request headers', request.headers);
 	}
 
