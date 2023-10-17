36eaf00d3e66187d019758b34fd8cd2cdfb3fcbc
Anton Cristo <antoncristo92@gmail.com>
2023-10-17 09:46:32 +0300

    get user service and hook

--- src/api/user/user.interface.ts
+++ src/api/user/user.interface.ts
@@ -1,6 +1,7 @@
-import { type SignInResponse } from '@cooke/types';
+import { type User, type SignInResponse } from '@cooke/types';
 
 export interface UserServiceApi {
 	login: (cred: Credentials) => Promise<SignInResponse>;
 	signUp: (cred: Credentials & { name: string }) => Promise<void>;
+	getUser: () => Promise<User>;
 }
