090b0cc7ac4f3352e27e18838658fb660416696c
Anton Cristo <antoncristo92@gmail.com>
2023-10-17 12:21:38 +0300

    test interceptor

--- src/api/axios/utils.ts
+++ src/api/axios/utils.ts
@@ -3,8 +3,12 @@ import { type InternalAxiosRequestConfig } from 'axios';
 
 export const setAuthBearer = (request: InternalAxiosRequestConfig<unknown>) => {
 	const accessToken = getSessionStorage('token');
+	// Fix: test
+	console.log('extracted token', accessToken);
 	if (accessToken) {
+		// Fix: test
 		request.headers.Authorization = `Bearer ${accessToken}`;
+		console.log('request headers', request.headers);
 	}
 
 	return request;
