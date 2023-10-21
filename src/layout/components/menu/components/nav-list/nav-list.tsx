879ee77d002f2717a59b71d3d8d88d83a7814134
Anton Cristo <antoncristo92@gmail.com>
2023-10-21 10:00:46 +0300

    dashboard page t function instead of hardocoded texts

--- src/layout/components/menu/components/nav-list/nav-list.tsx
+++ src/layout/components/menu/components/nav-list/nav-list.tsx
@@ -2,19 +2,24 @@ import { cookePathnames } from '@cooke/router';
 import { BookIcon, FavoritesIcon, WriteIcon } from '@cooke/assets';
 
 import * as Styled from './nav-list.styled';
+import { useTranslation } from 'react-i18next';
 
-export const NavList = () => (
-	<Styled.NavList>
-		<Styled.MenuLink to={cookePathnames.authenticatedPathNames.DASHBOARD}>
-			<BookIcon right='1rem' />
-			My Book
-		</Styled.MenuLink>
-		<Styled.MenuLink to={cookePathnames.authenticatedPathNames.CREATE_RECIPE} end>
-			<WriteIcon right='1rem' /> Add Recipe
-		</Styled.MenuLink>
-		<Styled.MenuLink to={cookePathnames.authenticatedPathNames.FAVORITES} end>
-			<FavoritesIcon right='0.6rem' />
-			Favorites
-		</Styled.MenuLink>
-	</Styled.NavList>
-);
+export const NavList = () => {
+	const { t } = useTranslation('dashboard', { keyPrefix: 'nav' });
+
+	return (
+		<Styled.NavList>
+			<Styled.MenuLink to={cookePathnames.authenticatedPathNames.DASHBOARD}>
+				<BookIcon right='1rem' />
+				{t('dashboard')}
+			</Styled.MenuLink>
+			<Styled.MenuLink to={cookePathnames.authenticatedPathNames.CREATE_RECIPE} end>
+				<WriteIcon right='1rem' /> {t('wizard')}
+			</Styled.MenuLink>
+			<Styled.MenuLink to={cookePathnames.authenticatedPathNames.FAVORITES} end>
+				<FavoritesIcon right='0.6rem' />
+				{t('favorites')}
+			</Styled.MenuLink>
+		</Styled.NavList>
+	);
+};
--- src/views/dashboard/components/initial-state/initial-state.tsx
+++ src/views/dashboard/components/initial-state/initial-state.tsx
@@ -1,13 +1,17 @@
 import { Loader } from '@cooke/shared';
 import * as Styled from './initial-state.styled';
 import { cookePathnames } from '@cooke/router';
+import { useTranslation } from 'react-i18next';
 
-export const InitialState = () => (
-	<Styled.InitialState>
-		<Loader size='M' />
-		<Styled.LinkButton to={cookePathnames.authenticatedPathNames.CREATE_RECIPE}>
-			LET`S START COOKING
-		</Styled.LinkButton>
-		<Loader size='M' />
-	</Styled.InitialState>
-);
+export const InitialState = () => {
+	const { t } = useTranslation('dashboard');
+	return (
+		<Styled.InitialState>
+			<Loader size='M' />
+			<Styled.LinkButton to={cookePathnames.authenticatedPathNames.CREATE_RECIPE}>
+				{t('initialState')}
+			</Styled.LinkButton>
+			<Loader size='M' />
+		</Styled.InitialState>
+	);
+};
