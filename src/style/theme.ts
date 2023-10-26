c498b03a7ba625db8b290bc8696cb25e7601ffcf
Anton Cristo <antoncristo92@gmail.com>
2023-10-26 12:03:41 +0300

    light mode theme template

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -44,15 +44,77 @@ export const cookeTheme: CookeTheme = {
 	},
 	decorators: {
 		boxShadow: {
-			primary: `0.1rem 0.1rem 1rem 0.1rem rgba(${Colors.BLACK}, 0.6)`,
+			primary: `0.1rem 0.1rem 0.6rem 0.1rem rgba(${Colors.BLACK}, 0.3)`,
 			secondary: `0.1rem 0.1rem 1rem 0.1rem rgba(${Colors.BLACK}, 0.3)`,
 			error: `0.1rem 0.1rem 1rem 0.1rem rgba(${Colors.ERROR}, 0.9)`
 		},
 		textShadow: {
-			primary: '0.1rem 0.1rem 0.6rem black'
+			primary: `0.1rem 0.1rem 0.6rem rgba(${Colors.BLACK}, 0.3)`
 		},
 		padding: {
 			default: '2rem'
 		}
 	}
 };
+
+/** Light mode
+ * import { Colors } from './colors';
+
+export type BaseColor =
+	| 'primary'
+	| 'secondary'
+	| 'black'
+	| 'white'
+	| 'grey'
+	| 'orange'
+	| 'error'
+	| 'warning'
+	| 'success';
+
+export interface Decorators {
+	boxShadow: {
+		primary: string;
+		secondary: string;
+		error: string;
+	};
+	textShadow: {
+		primary: string;
+	};
+	padding: {
+		default: string;
+	};
+}
+
+export interface CookeTheme {
+	colors: Record<BaseColor, Colors>;
+	decorators: Decorators;
+}
+
+export const cookeTheme: CookeTheme = {
+	colors: {
+		black: Colors.WHITE,
+		white: Colors.BLACK,
+		grey: Colors.GREY,
+		orange: Colors.ORANGE,
+		primary: Colors.SECONDARY,
+		secondary: Colors.WHITE,
+		success: Colors.SUCCESS,
+		warning: Colors.WARNING,
+		error: Colors.ERROR
+	},
+	decorators: {
+		boxShadow: {
+			primary: `0.1rem 0.1rem 0.6rem 0.1rem rgba(${Colors.BLACK}, 0.3)`,
+			secondary: `0.1rem 0.1rem 1rem 0.1rem rgba(${Colors.BLACK}, 0.3)`,
+			error: `0.1rem 0.1rem 1rem 0.1rem rgba(${Colors.ERROR}, 0.9)`
+		},
+		textShadow: {
+			primary: `0.1rem 0.1rem 0.6rem rgba(${Colors.BLACK}, 0.3)`
+		},
+		padding: {
+			default: '2rem'
+		}
+	}
+};
+
+ */
