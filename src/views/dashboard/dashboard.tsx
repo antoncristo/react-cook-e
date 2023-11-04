4e760e2aa6a186eded1ce81d70a413cdfb2e363b
Anton Cristo <antoncristo92@gmail.com>
2023-11-04 10:14:20 +0200

    previous fetching flow adapted to new search component behavior

--- src/views/dashboard/dashboard.tsx
+++ src/views/dashboard/dashboard.tsx
@@ -1,19 +1,25 @@
+import { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { recipesStore } from '@cooke/stores/recipes-store';
-import { Loader, NetworkError, Text } from '@cooke/shared';
+import { useGetRecipes } from '@cooke/api/recipe';
+import { Loader, NetworkError } from '@cooke/shared';
 import { Init } from '@cooke/init';
 
 import { RecipeCard, ControlBar, InitialState } from './components';
 
 import * as Styled from './dashboard.styled';
-import { useTranslation } from 'react-i18next';
 
 export const Dashboard = observer(() => {
-	const { t } = useTranslation('dashboard', { keyPrefix: 'controls' });
+	const { fetchedRecipes, isRecipesSuccess } = useGetRecipes({ enabled: true });
 	const { recipes } = recipesStore;
 
 	const isInitialState = recipes?.length === 0 && !recipesStore.searchQuery;
-	const isNoSearchMatch = recipes?.length === 0 && recipesStore.searchQuery;
+
+	useEffect(() => {
+		if (isRecipesSuccess) {
+			recipesStore.recipes = fetchedRecipes;
+		}
+	}, [isRecipesSuccess, fetchedRecipes]);
 
 	return (
 		<Init>
@@ -22,13 +28,6 @@ export const Dashboard = observer(() => {
 				<Styled.RecipesFlex>
 					{recipesStore.isLoading ? <Loader size='M' /> : null}
 					{recipesStore.isError ? <NetworkError /> : null}
-					{isNoSearchMatch ? (
-						<Text
-							fontSize='2rem'
-							numberOfLines={3}
-							text={t('noMatch', { input: recipesStore.searchQuery! })}
-						/>
-					) : null}
 					{isInitialState ? <InitialState /> : null}
 					{recipes?.map(recipe => (
 						<RecipeCard key={recipe.id} recipe={recipe} />
